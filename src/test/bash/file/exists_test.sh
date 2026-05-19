09d7bdd4a2239f506c6556f9f32e9fcb72741b15
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-20 00:57:41 +0300

    tests

---
+++ src/test/bash/file/exists_test.sh
@@ -0,0 +1,39 @@
+#!/usr/local/bin/bash
+
+SCRIPT="src/main/bash/file/exists.sh"
+
+if [[ ! -e "${SCRIPT}" ]]; then
+ echo "No file \"${SCRIPT}\"!" >&2; exit 1
+elif [[ -L "${SCRIPT}" ]]; then
+ echo "The \"${SCRIPT}\" is symlink!" >&2; exit 1
+elif [[ ! -f "${SCRIPT}" ]]; then
+ echo "Not a regular file \"${SCRIPT}\"!" >&2; exit 1
+elif [[ ! -s "${SCRIPT}" ]]; then
+ echo "File \"${SCRIPT}\" is empty!" >&2; exit 1
+elif [[ ! -x "${SCRIPT}" ]]; then
+ echo "File \"${SCRIPT}\" is not executable!" >&2; exit 1
+fi
+
+ACTUAL_VALUE="$(${SCRIPT} '' 2>&1)"; CODE=$?
+if test "${CODE}" != '1'; then
+ echo "Code(${CODE}) error!" >&2; exit 1
+elif test "${ACTUAL_VALUE}" != 'Wrong arguments!'; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
+fi
+
+ACTUAL_VALUE="$(${SCRIPT} 2>&1)"; CODE=$?
+if test "${CODE}" != '1'; then
+ echo "Code(${CODE}) error!" >&2; exit 1
+elif test "${ACTUAL_VALUE}" != 'No issuer!'; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
+fi
+
+POINTER="$(date +%s)"
+rm -rf "/tmp/${POINTER}"
+
+ACTUAL_VALUE="$(ISSUER="/tmp/${POINTER}" ${SCRIPT} 2>&1)"; CODE=$?
+if test "${CODE}" != '1'; then
+ echo "Code(${CODE}) error!" >&2; exit 1
+elif test "${ACTUAL_VALUE}" != "No file \"/tmp/${POINTER}\"!"; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
+fi
