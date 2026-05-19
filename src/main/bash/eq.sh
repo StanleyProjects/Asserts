a1e9f9d8857ad0a2f3a482b17abe1786d29a6958
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-20 00:29:21 +0300

    wip

--- src/main/bash/eq.sh
+++ src/main/bash/eq.sh
@@ -1,17 +1,16 @@
 #!/usr/local/bin/bash
 
 if test $# -ne 2; then
- echo 'Wrong arguments!'; exit 1; fi
+ echo 'Wrong arguments!' >&2; exit 1; fi
 
 if test -z "${ISSUER}"; then
- echo 'No issuer!'; exit 1; fi
+ echo 'No issuer!' >&2; exit 1; fi
 
 VALUE_ACTUAL="$1"
 VALUE_EXPECTED="$2"
 
 if [ "${VALUE_ACTUAL}" != "${VALUE_EXPECTED}" ]; then
- echo "Issuer \"${ISSUER}\" error!"
- echo "Actual(${#VALUE_ACTUAL}): \"${VALUE_ACTUAL}\""
- echo "Expected(${#VALUE_EXPECTED}): \"${VALUE_EXPECTED}\""
- exit 1
-fi
+ echo -n "Issuer \"${ISSUER}\" error!
+Actual(${#VALUE_ACTUAL}): \"${VALUE_ACTUAL}\"
+Expected(${#VALUE_EXPECTED}): \"${VALUE_EXPECTED}
+" >&2; exit 1; fi
