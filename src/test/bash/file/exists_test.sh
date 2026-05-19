12bee53112dedd9d1e967c1aff798f456e09bef5
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-20 01:19:48 +0300

    wip

--- src/test/bash/file/exists_test.sh
+++ src/test/bash/file/exists_test.sh
@@ -28,39 +28,35 @@ elif test "${ACTUAL_VALUE}" != 'No issuer!'; then
  echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
 fi
 
-POINTER="$(date +%s)"
-rm -rf "/tmp/${POINTER}"
+TMP_DIR="$(mktemp -d)" || exit 1
+trap 'rm -rf "${TMP_DIR}"' EXIT
 
-ACTUAL_VALUE="$(ISSUER="/tmp/${POINTER}" ${SCRIPT} 2>&1)"; CODE=$?
+ACTUAL_VALUE="$(ISSUER="${TMP_DIR}/foo" ${SCRIPT} 2>&1)"; CODE=$?
 if test "${CODE}" != '1'; then
  echo "Code(${CODE}) error!" >&2; exit 1
-elif test "${ACTUAL_VALUE}" != "No file \"/tmp/${POINTER}\"!"; then
+elif test "${ACTUAL_VALUE}" != "No file \"${TMP_DIR}/foo\"!"; then
  echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
 fi
 
-mkdir "/tmp/${POINTER}"
-ACTUAL_VALUE="$(ISSUER="/tmp/${POINTER}" ${SCRIPT} 2>&1)"; CODE=$?
+ACTUAL_VALUE="$(ISSUER="${TMP_DIR}" ${SCRIPT} 2>&1)"; CODE=$?
 if test "${CODE}" != '1'; then
  echo "Code(${CODE}) error!" >&2; exit 1
-elif test "${ACTUAL_VALUE}" != "Not a regular file \"/tmp/${POINTER}\"!"; then
+elif test "${ACTUAL_VALUE}" != "Not a regular file \"${TMP_DIR}\"!"; then
  echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
 fi
-rm -rf "/tmp/${POINTER}"
 
-touch "/tmp/${POINTER}"
-ln -s "/tmp/${POINTER}" "/tmp/${POINTER}.link"
-ACTUAL_VALUE="$(ISSUER="/tmp/${POINTER}.link" ${SCRIPT} 2>&1)"; CODE=$?
+touch "${TMP_DIR}/foo"
+ln -s "${TMP_DIR}/foo" "${TMP_DIR}/link"
+ACTUAL_VALUE="$(ISSUER="${TMP_DIR}/link" ${SCRIPT} 2>&1)"; CODE=$?
 if test "${CODE}" != '1'; then
  echo "Code(${CODE}) error!" >&2; exit 1
-elif test "${ACTUAL_VALUE}" != "The \"/tmp/${POINTER}.link\" is symlink!"; then
+elif test "${ACTUAL_VALUE}" != "The \"${TMP_DIR}/link\" is symlink!"; then
  echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
 fi
-rm "/tmp/${POINTER}.link"
 
-ACTUAL_VALUE="$(ISSUER="/tmp/${POINTER}" ${SCRIPT} 2>&1)"; CODE=$?
+ACTUAL_VALUE="$(ISSUER="${TMP_DIR}/foo" ${SCRIPT} 2>&1)"; CODE=$?
 if test "${CODE}" != '0'; then
  echo "Code(${CODE}) error!" >&2; exit 1
 elif test "${ACTUAL_VALUE}" != ''; then
  echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
 fi
-rm "/tmp/${POINTER}"
