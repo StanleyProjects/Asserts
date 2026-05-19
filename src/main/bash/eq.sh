e8daee5af71e6510e6f99e690b17d7b329263337
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-20 00:32:46 +0300

    files

--- src/main/bash/eq.sh
+++ src/main/bash/eq.sh
@@ -9,8 +9,8 @@ if test -z "${ISSUER}"; then
 VALUE_ACTUAL="$1"
 VALUE_EXPECTED="$2"
 
-if [ "${VALUE_ACTUAL}" != "${VALUE_EXPECTED}" ]; then
+if [[ "${VALUE_ACTUAL}" != "${VALUE_EXPECTED}" ]]; then
  echo -n "Issuer \"${ISSUER}\" error!
 Actual(${#VALUE_ACTUAL}): \"${VALUE_ACTUAL}\"
-Expected(${#VALUE_EXPECTED}): \"${VALUE_EXPECTED}
+Expected(${#VALUE_EXPECTED}): \"${VALUE_EXPECTED}\"
 " >&2; exit 1; fi
