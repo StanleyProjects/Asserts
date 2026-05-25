b67792126368d7d6438126be7a3b8fe19137cd0f
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-25 23:39:05 +0300

    fix tests

--- src/test/bash/file/filled_test.sh
+++ src/test/bash/file/filled_test.sh
@@ -31,7 +31,7 @@ if [[ "${ACTUAL_VALUE}" != 'Wrong arguments!' ]]; then
 if [[ "${CODE}" != '1' ]]; then
  echo "Code(${CODE}) error!" >&2; exit 1; fi
 ACTUAL_VALUE="$(<"${STDERR}")"
-if [[ "${ACTUAL_VALUE}" != 'No tested path!' ]]; then
+if [[ "${ACTUAL_VALUE}" != 'No assert path!' ]]; then
  echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
 
 :> "${STDERR}"
