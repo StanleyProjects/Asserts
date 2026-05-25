27c473bfe4f904eb2c01ebe22af6a8e495d47ea5
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-25 21:38:58 +0300

    tests

--- src/test/bash/eq_test.sh
+++ src/test/bash/eq_test.sh
@@ -43,35 +43,32 @@ if test "${ACTUAL_VALUE}" != 'Wrong arguments!'; then
 
 :> "${STDERR}"
 
-ACTUAL_VALUE="$(${SCRIPT} '' '' 2>&1)"; CODE=$?
+"${SCRIPT}" '' '' 2>"${STDERR}"; CODE=$?
 if test "${CODE}" != '1'; then
- echo "Code(${CODE}) error!" >&2; exit 1
-elif test "${ACTUAL_VALUE}" != 'No issuer!'; then
- echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
-fi
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
+ACTUAL_VALUE="$(<"${STDERR}")"
+if test "${ACTUAL_VALUE}" != 'No issuer!'; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
 
-ACTUAL_VALUE="$(ISSUER='foo' ${SCRIPT} 'a' 'b' 2>&1)"; CODE=$?
+:> "${STDERR}"
+
+ISSUER='foo' "${SCRIPT}" 'a' 'b' 2>"${STDERR}"; CODE=$?
+if test "${CODE}" != '1'; then
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
 EXPECTED_VALUE='Issuer "foo" error!
 Actual(1): "a"
 Expected(1): "b"'
-if test "${CODE}" != '1'; then
- echo "Code(${CODE}) error!" >&2; exit 1
-elif test "${ACTUAL_VALUE}" != "${EXPECTED_VALUE}"; then
- echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
-fi
+ACTUAL_VALUE="$(<"${STDERR}")"
+if test "${ACTUAL_VALUE}" != "${EXPECTED_VALUE}"; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
 
-ACTUAL_VALUE="$(ISSUER='foo' ${SCRIPT} 'value' 'value' 2>&1)"; CODE=$?
-if test "${CODE}" != '0'; then
- echo "Code(${CODE}) error!" >&2; exit 1
-elif test "${ACTUAL_VALUE}" != ''; then
- echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
-fi
+:> "${STDERR}"
 
-ACTUAL_VALUE="$(ISSUER='foo' ${SCRIPT} '' '' 2>&1)"; CODE=$?
+ISSUER='foo' "${SCRIPT}" 'a' 'a' 2>"${STDERR}"; CODE=$?
 if test "${CODE}" != '0'; then
- echo "Code(${CODE}) error!" >&2; exit 1
-elif test "${ACTUAL_VALUE}" != ''; then
- echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
-fi
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
+ACTUAL_VALUE="$(<"${STDERR}")"
+if test -n "${ACTUAL_VALUE}"; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
 
 rm "${STDERR}"
