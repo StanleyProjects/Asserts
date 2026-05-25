be891f63ee1fc99ee0ebdbf09b365ad23d6ccc4c
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-25 21:44:01 +0300

    tests

--- src/test/bash/ne_test.sh
+++ src/test/bash/ne_test.sh
@@ -14,55 +14,60 @@ elif [[ ! -x "${SCRIPT}" ]]; then
  echo "File \"${SCRIPT}\" is not executable!" >&2; exit 1
 fi
 
-ACTUAL_VALUE="$(${SCRIPT} 2>&1)"; CODE=$?
-if test "${CODE}" != '1'; then
- echo "Code(${CODE}) error!" >&2; exit 1
-elif test "${ACTUAL_VALUE}" != 'Wrong arguments!'; then
- echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
-fi
+STDERR="$(mktemp)"
 
-ACTUAL_VALUE="$(${SCRIPT} '' 2>&1)"; CODE=$?
+"${SCRIPT}" 2>"${STDERR}"; CODE=$?
 if test "${CODE}" != '1'; then
- echo "Code(${CODE}) error!" >&2; exit 1
-elif test "${ACTUAL_VALUE}" != 'Wrong arguments!'; then
- echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
-fi
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
+ACTUAL_VALUE="$(<"${STDERR}")"
+if test "${ACTUAL_VALUE}" != 'Wrong arguments!'; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
 
-ACTUAL_VALUE="$(${SCRIPT} '' '' '' 2>&1)"; CODE=$?
+:> "${STDERR}"
+
+"${SCRIPT}" '' 2>"${STDERR}"; CODE=$?
 if test "${CODE}" != '1'; then
- echo "Code(${CODE}) error!" >&2; exit 1
-elif test "${ACTUAL_VALUE}" != 'Wrong arguments!'; then
- echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
-fi
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
+ACTUAL_VALUE="$(<"${STDERR}")"
+if test "${ACTUAL_VALUE}" != 'Wrong arguments!'; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
 
-ACTUAL_VALUE="$(${SCRIPT} '' '' 2>&1)"; CODE=$?
+:> "${STDERR}"
+
+"${SCRIPT}" '' '' '' 2>"${STDERR}"; CODE=$?
 if test "${CODE}" != '1'; then
- echo "Code(${CODE}) error!" >&2; exit 1
-elif test "${ACTUAL_VALUE}" != 'No issuer!'; then
- echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
-fi
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
+ACTUAL_VALUE="$(<"${STDERR}")"
+if test "${ACTUAL_VALUE}" != 'Wrong arguments!'; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
 
-ACTUAL_VALUE="$(ISSUER='foo' ${SCRIPT} 'a' 'a' 2>&1)"; CODE=$?
+:> "${STDERR}"
+
+"${SCRIPT}" '' '' 2>"${STDERR}"; CODE=$?
+if test "${CODE}" != '1'; then
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
+ACTUAL_VALUE="$(<"${STDERR}")"
+if test "${ACTUAL_VALUE}" != 'No issuer!'; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
+
+:> "${STDERR}"
+
+ISSUER='foo' "${SCRIPT}" 'a' 'a' 2>"${STDERR}"; CODE=$?
+if test "${CODE}" != '1'; then
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
 EXPECTED_VALUE='Issuer "foo" error!
 Values(1) equal: "a"'
-if test "${CODE}" != '1'; then
- echo "Code(${CODE}) error!" >&2; exit 1
-elif test "${ACTUAL_VALUE}" != "${EXPECTED_VALUE}"; then
- echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
-fi
+ACTUAL_VALUE="$(<"${STDERR}")"
+if test "${ACTUAL_VALUE}" != "${EXPECTED_VALUE}"; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
 
-ACTUAL_VALUE="$(ISSUER='foo' ${SCRIPT} '' '' 2>&1)"; CODE=$?
-EXPECTED_VALUE='Issuer "foo" error!
-Values(0) equal: ""'
-if test "${CODE}" != '1'; then
- echo "Code(${CODE}) error!" >&2; exit 1
-elif test "${ACTUAL_VALUE}" != "${EXPECTED_VALUE}"; then
- echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
-fi
+:> "${STDERR}"
 
-ACTUAL_VALUE="$(ISSUER='foo' ${SCRIPT} 'a' 'b' 2>&1)"; CODE=$?
+ISSUER='foo' "${SCRIPT}" 'a' 'b' 2>"${STDERR}"; CODE=$?
 if test "${CODE}" != '0'; then
- echo "Code(${CODE}) error!" >&2; exit 1
-elif test "${ACTUAL_VALUE}" != ''; then
- echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1
-fi
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
+ACTUAL_VALUE="$(<"${STDERR}")"
+if test -n "${ACTUAL_VALUE}"; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
+
+rm "${STDERR}"
