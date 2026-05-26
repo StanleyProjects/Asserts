43bb7d587714691ec381c39afc10c0536a2ea124
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-27 00:26:46 +0300

    wip

--- src/main/bash/contains.sh
+++ src/main/bash/contains.sh
@@ -3,24 +3,24 @@
 if [[ $# -ne 3 ]]; then
  echo 'Wrong arguments!' >&2; exit 1; fi
 
-ASSERT_CONTEXT="$1"
+ASSERTS_CONTEXT="$1"
 
-if [[ -z "${ASSERT_CONTEXT}" ]]; then
- echo 'No assert context!' >&2; exit 1; fi
+if [[ -z "${ASSERTS_CONTEXT}" ]]; then
+ echo 'No context!' >&2; exit 1; fi
 
-ACTUAL_TEXT="$2"
-EXPECTED_SUBTEXT="$3"
+ASSERTS_TEXT="$2"
+ASSERTS_SUBTEXT="$3"
 
-if [[ -z "${EXPECTED_SUBTEXT}" ]]; then
- echo 'No expected subtext!' >&2; exit 1; fi
+if [[ -z "${ASSERTS_SUBTEXT}" ]]; then
+ echo 'No subtext!' >&2; exit 1; fi
 
-if [[ "${ACTUAL_TEXT}" != *"${EXPECTED_SUBTEXT}"* ]]; then
- printf '%s' "Assert context: \"${ASSERT_CONTEXT}\"
----(${#ACTUAL_TEXT})
-${ACTUAL_TEXT}
+if [[ "${ASSERTS_TEXT}" != *"${ASSERTS_SUBTEXT}"* ]]; then
+ printf '%s' "Context: \"${ASSERTS_CONTEXT}\"
+---(${#ASSERTS_TEXT})
+${ASSERTS_TEXT}
 ---
 ...does not contain:
----(${#EXPECTED_SUBTEXT})
-${EXPECTED_SUBTEXT}
+---(${#ASSERTS_SUBTEXT})
+${ASSERTS_SUBTEXT}
 ---
 " >&2; exit 1; fi
