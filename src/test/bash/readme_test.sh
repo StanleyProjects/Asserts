cfa3cf3350a4e11d14966ea36e17c7c51de6492c
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-27 00:24:07 +0300

    tests

--- src/test/bash/readme_test.sh
+++ src/test/bash/readme_test.sh
@@ -18,13 +18,32 @@ elif [[ ! -s "${ISSUER}" ]]; then
  echo "File \"${ISSUER}\" is empty!"; exit 1
 fi
 
-EXPECTED_TEXT="\`${VERSION}\`
+EXPECTED_RELEASE="
+\`${VERSION}\`
 | [GitHub](https://github.com/${REP_OWNER}/${REP_NAME}/releases/tag/${VERSION})
 | [Key](https://${REP_OWNER}.github.io/release-public.pem"
 
-ALL_TEXT="$(< "${ISSUER}")"
-if [[ "${ALL_TEXT}" != *"${EXPECTED_TEXT}"* ]]; then
- echo "File \"${ISSUER}\" does not contain:
----
-${EXPECTED_TEXT}
----"; exit 1; fi
+EXPECTED_BUILD_AND_INSTALL="
+$ ./assemble.sh \\
+ && ./src/test/bash/unit_test.sh \\
+ && unzip -d /opt/${REP_NAME}-${VERSION} ./build/zip/${REP_NAME}-${VERSION}.zip"
+
+EXPECTED_DOWNLOAD_AND_INSTALL="
+$ TMP_PATH=\"\$(mktemp)\"; \\
+ curl -L 'https://github.com/${REP_OWNER}/${REP_NAME}/releases/download/${VERSION}/${REP_NAME}-${VERSION}.zip' \\
+  -o \"\${TMP_PATH}\" && unzip -d /opt/${REP_NAME}-${VERSION} \"\${TMP_PATH}\" && rm \"\${TMP_PATH}\""
+
+EXPECTED_TEXTS=(
+ "${EXPECTED_RELEASE}"
+ "${EXPECTED_BUILD_AND_INSTALL}"
+ "${EXPECTED_DOWNLOAD_AND_INSTALL}"
+)
+
+for EXPECTED_TEXT in "${EXPECTED_TEXTS[@]}"; do
+ ALL_TEXT="$(< "${ISSUER}")"
+ if [[ "${ALL_TEXT}" != *"${EXPECTED_TEXT}"* ]]; then
+  echo "File \"${ISSUER}\" does not contain:
+ ---
+ ${EXPECTED_TEXT}
+ ---"; exit 1; fi
+done
