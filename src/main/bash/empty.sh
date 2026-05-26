7370937e2a4afe0e94c5375aa6360e99c5896d26
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-27 01:24:54 +0300

    empty

---
+++ src/main/bash/empty.sh
@@ -0,0 +1,18 @@
+#!/usr/local/bin/bash
+
+if [[ $# -ne 3 ]]; then
+ echo 'Wrong arguments!' >&2; exit 1; fi
+
+ASSERTS_CONTEXT="$1"
+
+if [[ -z "${ASSERTS_CONTEXT}" ]]; then
+ echo 'No context!' >&2; exit 1; fi
+
+ASSERTS_TEXT="$2"
+
+if [[ -n "${ASSERTS_TEXT}" ]]; then
+ printf '%s' "Context: \"${ASSERTS_CONTEXT}\"
+---(${#ASSERTS_TEXT})
+${ASSERTS_TEXT}
+---
+" >&2; exit 1; fi
