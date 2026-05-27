88497a01dfc9afc1a4bdc3ba4942ca920d28fa99
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-27 23:27:34 +0300

    not exists

---
+++ src/main/bash/file/not/exists.sh
@@ -0,0 +1,18 @@
+#!/usr/local/bin/bash
+
+if [[ $# -ne 1 ]]; then
+ echo 'Wrong arguments!' >&2; exit 1; fi
+
+ASSERTS_PATH="$1"
+
+if [[ -z "${ASSERTS_PATH}" ]]; then
+ echo 'No path!' >&2; exit 1
+elif [[ -L "${ASSERTS_PATH}" ]]; then
+ echo "The \"${ASSERTS_PATH}\" is symlink!" >&2; exit 1
+elif [[ -e "${ASSERTS_PATH}" ]]; then
+ if [[ ! -f "${ASSERTS_PATH}" ]]; then
+  echo "Not a regular file \"${ASSERTS_PATH}\"!" >&2; exit 1
+ else
+  echo "File \"${ASSERTS_PATH}\" exists!" >&2; exit 1
+ fi
+fi
