e398ab81d4c8e8856916c873108336e80f86b912
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-27 23:29:19 +0300

    not empty

---
+++ src/main/bash/not/empty.sh
@@ -0,0 +1,15 @@
+#!/usr/local/bin/bash
+
+if [[ $# -ne 2 ]]; then
+ echo 'Wrong arguments!' >&2; exit 1; fi
+
+ASSERTS_CONTEXT="$1"
+
+if [[ -z "${ASSERTS_CONTEXT}" ]]; then
+ echo 'No context!' >&2; exit 1; fi
+
+ASSERTS_TEXT="$2"
+
+if [[ -z "${ASSERTS_TEXT}" ]]; then
+ echo "Context: \"${ASSERTS_CONTEXT}\"
+Value is empty!" >&2; exit 1; fi
