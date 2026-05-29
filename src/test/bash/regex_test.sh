a439506a5edcb7f46816230d4e9b05e897ddf51b
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-30 00:12:15 +0300

    tests

--- src/test/bash/regex_test.sh
+++ src/test/bash/regex_test.sh
@@ -70,6 +70,8 @@ ACTUAL_VALUE="$(<"${STDERR}")"
 if [[ "${ACTUAL_VALUE}" != 'No regex!' ]]; then
  echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
 
+# Expects "Invalid regex" for malformed ERE patterns.
+
 ASSERTS_REGEXES=('(' '[' '|' '[z-a]' '*' '**')
 for ASSERTS_REGEX in "${ASSERTS_REGEXES[@]}"; do
  :> "${STDERR}"
@@ -88,6 +90,8 @@ done
 
 :> "${STDERR}"
 
+# Expects mismatch output when text does not match anchored regex.
+
 "${SCRIPT}" '42' 'foo' '^bar$' 2>"${STDERR}"; CODE=$?
 if [[ "${CODE}" != '1' ]]; then
  echo "Code(${CODE}) error!" >&2; exit 1; fi
@@ -103,6 +107,8 @@ ACTUAL_VALUE="$(<"${STDERR}")"
 if [[ "${ACTUAL_VALUE}" != "${EXPECTED_VALUE}" ]]; then
  echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
 
+# Expects success for each text containing "foo" under ^.*foo.*$.
+
 ACTUAL_TEXTS=('foo' ' foo' 'foo ' ' foo ' 'foo foo' 'foo bar' 'qux foo bar' 'qux\nfoo\nbar' $'qux\nfoo\nbar')
 for ASSERTS_TEXT in "${ACTUAL_TEXTS[@]}"; do
  :> "${STDERR}"
@@ -114,6 +120,12 @@ for ASSERTS_TEXT in "${ACTUAL_TEXTS[@]}"; do
   echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
 done
 
+# Expects mismatch for texts against self.
+# FOO='\n' "${FOO}" =~ ${FOO} -> \n contains n
+# FOO=$'\n' "${FOO}" =~ ${FOO} -> $'\n' contains $'\n'
+# FOO='a\n' "${FOO}" =~ ${FOO} -> a\n not contains an
+# FOO=$'a\n' "${FOO}" =~ ${FOO} -> a$'\n' contains a$'\n'
+
 ACTUAL_TEXTS=('a' ')' ']' '\n' $'\n' $'a\n')
 for ASSERTS_TEXT in "${ACTUAL_TEXTS[@]}"; do
  :> "${STDERR}"
@@ -124,6 +136,8 @@ for ASSERTS_TEXT in "${ACTUAL_TEXTS[@]}"; do
  if [[ -n "${ACTUAL_VALUE}" ]]; then
   echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
 done
+
+# Expects mismatch for non-empty one-char texts against ^$.
 
 ACTUAL_TEXTS=(' ' $'\n' $'\t')
 for ASSERTS_TEXT in "${ACTUAL_TEXTS[@]}"; do
@@ -144,6 +158,8 @@ does not satisfy the regex:
   echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
 done
 
+# Expects success for empty text with regexes that allow emptiness.
+
 ASSERTS_REGEXES=('.*' '^$')
 for ASSERTS_REGEX in "${ASSERTS_REGEXES[@]}"; do
  :> "${STDERR}"
@@ -157,8 +173,11 @@ done
 
 :> "${STDERR}"
 
-ASSERTS_REGEX='(^|\n)[1-4]{1}\n[5-7]{2}($|\n)'
+# Expects mismatch when text has real LF but regex contains literal \n.
+# FOO=$'\n1' BAR='\n1' "${FOO}" =~ ${BAR} -> $'\n'1 not satisfy {\n}1
+
 ASSERTS_TEXT=$'foo\n1\n55\nbaz'
+ASSERTS_REGEX='(^|\n)[1-4]{1}\n[5-7]{2}($|\n)'
 "${SCRIPT}" '42' "${ASSERTS_TEXT}" "${ASSERTS_REGEX}" 2>"${STDERR}"; CODE=$?
 if [[ "${CODE}" != '1' ]]; then
  echo "Code(${CODE}) error!" >&2; exit 1; fi
@@ -179,8 +198,32 @@ if [[ "${ACTUAL_VALUE}" != "${EXPECTED_VALUE}" ]]; then
 
 :> "${STDERR}"
 
-ASSERTS_REGEX=$'(^|\n)[1-4]{1}\n[5-7]{2}($|\n)'
+# FOO='\n1' BAR='\n1' "${FOO}" =~ ${BAR} -> \n1 not satisfy {\n}1
+
+ASSERTS_TEXT='foo\n1\n55\nbaz'
+ASSERTS_REGEX='(^|\n)[1-4]{1}\n[5-7]{2}($|\n)'
+"${SCRIPT}" '42' "${ASSERTS_TEXT}" "${ASSERTS_REGEX}" 2>"${STDERR}"; CODE=$?
+if [[ "${CODE}" != '1' ]]; then
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
+EXPECTED_VALUE='Context: "42"
+---(15)
+foo\n1\n55\nbaz
+---
+does not satisfy the regex:
+---(30)
+(^|\n)[1-4]{1}\n[5-7]{2}($|\n)
+---'
+ACTUAL_VALUE="$(<"${STDERR}")"
+if [[ "${ACTUAL_VALUE}" != "${EXPECTED_VALUE}" ]]; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
+
+:> "${STDERR}"
+
+# Expects success when both text and regex contain real LF characters.
+# FOO=$'\n1' BAR=$'\n1' "${FOO}" =~ ${BAR} -> $'\n'1 satisfy $'\n'1
+
 ASSERTS_TEXT=$'foo\n1\n55\nbaz'
+ASSERTS_REGEX=$'(^|\n)[1-4]{1}\n[5-7]{2}($|\n)'
 "${SCRIPT}" '42' "${ASSERTS_TEXT}" "${ASSERTS_REGEX}" 2>"${STDERR}"; CODE=$?
 if [[ "${CODE}" != '0' ]]; then
  echo "Code(${CODE}) error!" >&2; exit 1; fi
@@ -190,8 +233,11 @@ if [[ -n "${ACTUAL_VALUE}" ]]; then
 
 :> "${STDERR}"
 
-ASSERTS_REGEX='(^|\\n)[1-4]{1}\\n[5-7]{2}($|\\n)'
+# Expects success when both text and regex use literal backslash-n.
+# FOO='\n1' BAR='\\n1' "${FOO}" =~ ${BAR} -> \n1 satisfy \n1
+
 ASSERTS_TEXT='foo\n1\n55\nbaz'
+ASSERTS_REGEX='(^|\\n)[1-4]{1}\\n[5-7]{2}($|\\n)'
 "${SCRIPT}" '42' "${ASSERTS_TEXT}" "${ASSERTS_REGEX}" 2>"${STDERR}"; CODE=$?
 if [[ "${CODE}" != '0' ]]; then
  echo "Code(${CODE}) error!" >&2; exit 1; fi
