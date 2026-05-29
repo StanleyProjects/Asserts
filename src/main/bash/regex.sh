231056a80cff5bdaf9a7c569c2bc1589737ee0f5
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-29 15:24:46 +0300

    regex

--- src/main/bash/regex.sh
+++ src/main/bash/regex.sh
@@ -14,13 +14,23 @@ ASSERTS_REGEX="$3"
 if [[ -z "${ASSERTS_REGEX}" ]]; then
  echo 'No regex!' >&2; exit 1; fi
 
-if [[ ! "${ASSERTS_TEXT}" =~ ${ASSERTS_REGEX} ]]; then
- printf '%s' "Context: \"${ASSERTS_CONTEXT}\"
+[[ "${ASSERTS_TEXT}" =~ ${ASSERTS_REGEX} ]]; CODE=$?
+
+if [[ "${CODE}" == '2' ]]; then
+ echo "Context: \"${ASSERTS_CONTEXT}\"
+Invalid regex:
+---(${#ASSERTS_REGEX})
+${ASSERTS_REGEX}
+---" >&2
+ exit 1
+elif [[ "${CODE}" != '0' ]]; then
+ echo "Context: \"${ASSERTS_CONTEXT}\"
 ---(${#ASSERTS_TEXT})
 ${ASSERTS_TEXT}
 ---
 does not satisfy the regex:
 ---(${#ASSERTS_REGEX})
 ${ASSERTS_REGEX}
----
-" >&2; exit 1; fi
+---" >&2
+ exit 1
+fi
