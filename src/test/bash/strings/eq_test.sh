817d09c833fe7a3f684c00145386ef917e144b33
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-30 13:54:05 +0300

    wip

--- src/test/bash/strings/eq_test.sh
+++ src/test/bash/strings/eq_test.sh
@@ -1,19 +1,21 @@
 #!/usr/local/bin/bash
 
-SCRIPT="src/main/bash/eq.sh"
+SCRIPT="src/main/bash/strings/eq.sh"
 
 echo "Running test of \"${SCRIPT}\"..."
 
-if [[ ! -e "${SCRIPT}" ]]; then
+if [[ -L "${SCRIPT}" ]]; then
+ echo "\"${SCRIPT}\" is a symlink!" >&2; exit 1
+elif [[ ! -e "${SCRIPT}" ]]; then
  echo "No file \"${SCRIPT}\"!" >&2; exit 1
-elif [[ -L "${SCRIPT}" ]]; then
- echo "The \"${SCRIPT}\" is symlink!" >&2; exit 1
 elif [[ ! -f "${SCRIPT}" ]]; then
- echo "Not a regular file \"${SCRIPT}\"!" >&2; exit 1
+ echo "\"${SCRIPT}\" is not a regular file!" >&2; exit 1
 elif [[ ! -s "${SCRIPT}" ]]; then
- echo "File \"${SCRIPT}\" is empty!" >&2; exit 1
+ echo "\"${SCRIPT}\" is empty!" >&2; exit 1
 elif [[ ! -x "${SCRIPT}" ]]; then
- echo "File \"${SCRIPT}\" is not executable!" >&2; exit 1
+ echo "\"${SCRIPT}\" is not executable!" >&2; exit 1
+elif ! bash -n "${SCRIPT}" ]]; then
+ echo "\"${SCRIPT}\" has wrong syntax!" >&2; exit 1
 fi
 
 STDERR="$(mktemp)"
@@ -37,6 +39,15 @@ if [[ "${ACTUAL_VALUE}" != 'Wrong arguments!' ]]; then
 :> "${STDERR}"
 
 "${SCRIPT}" '' '' 2>"${STDERR}"; CODE=$?
+if [[ "${CODE}" != '1' ]]; then
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
+ACTUAL_VALUE="$(<"${STDERR}")"
+if [[ "${ACTUAL_VALUE}" != 'Wrong arguments!' ]]; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
+
+:> "${STDERR}"
+
+"${SCRIPT}" '' '' '' '' 2>"${STDERR}"; CODE=$?
 if [[ "${CODE}" != '1' ]]; then
  echo "Code(${CODE}) error!" >&2; exit 1; fi
 ACTUAL_VALUE="$(<"${STDERR}")"
