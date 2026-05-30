e26f4c25ac3e9206fb09e8db454e36a97c6a664f
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-30 14:54:23 +0300

    locale

--- src/main/bash/strings/regex.sh
+++ src/main/bash/strings/regex.sh
@@ -1,7 +1,19 @@
 #!/usr/local/bin/bash
 
-if [[ $# -ne 3 ]]; then
- echo 'Wrong arguments!' >&2; exit 1; fi
+if [[ $# -eq 4 ]]; then
+ ASSERTS_LOCALE="$4"
+elif [[ $# -ne 3 ]]; then
+ echo 'Wrong arguments!' >&2; exit 1
+else
+ ASSERTS_LOCALE='C'
+fi
+
+if LC_ALL="${ASSERTS_LOCALE}" locale charmap >/dev/null 2>&1 \
+ && locale -a | grep -Fx -- "${ASSERTS_LOCALE}" >/dev/null 2>&1; then
+ LC_ALL="${ASSERTS_LOCALE}"
+else
+ echo "Locale \"${ASSERTS_LOCALE}\" is not supported!" >&2; exit 1
+fi
 
 ASSERTS_CONTEXT="$1"
 
