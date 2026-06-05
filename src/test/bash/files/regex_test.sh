8fc099c164d2e37c8ee4cdb2561293656362e775
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-05 22:17:31 +0300

    tests

--- src/test/bash/files/regex_test.sh
+++ src/test/bash/files/regex_test.sh
@@ -182,6 +182,26 @@ for ACTUAL_TEXT in "${ACTUAL_TEXTS[@]}"; do
   echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
 done
 
+# Expects mismatch for non-empty one-char texts against ^$.
+
+ACTUAL_TEXTS=(' ' $'\t')
+ASSERTS_REGEX='^$'
+for ACTUAL_TEXT in "${ACTUAL_TEXTS[@]}"; do
+ :> "${STDERR}"
+ printf '%s' "${ACTUAL_TEXT}" > "${TMP_PATH}"
+ "${SCRIPT}" "${TMP_PATH}" "${ASSERTS_REGEX}" 2>"${STDERR}"; CODE=$?
+ if [[ "${CODE}" != '1' ]]; then
+  echo "Code(${CODE}) error!" >&2; exit 1; fi
+ EXPECTED_VALUE="\"${TMP_PATH}\"
+does not satisfy the regex:
+---(${#ASSERTS_REGEX})
+${ASSERTS_REGEX}
+---"
+ ACTUAL_VALUE="$(<"${STDERR}")"
+ if [[ "${ACTUAL_VALUE}" != "${EXPECTED_VALUE}" ]]; then
+  echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
+done
+
 echo 'Not implemented!'; exit 1 # todo
 
 ASSERTS_SUBTEXT='foo'
